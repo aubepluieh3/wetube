fe4f5682494fa31d314010eefce4ae7783569f99
SOO <66086544+aubepluieh3@users.noreply.github.com>
2022-09-06 13:23:52 +0900

    #4.5 Conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,15 @@
-export const trending = (req,res) => res.render("home", {pageTitle: "Home"});
-export const see = (req,res)=> res.render("watch");
-export const edit = (req,res)=> res.render("edit");
-export const search = (req,res)=> res.send("Search");
-export const upload = (req,res)=> res.send("Upload");
-export const deleteVideo = (req,res)=> {
-    console.log(req.params);
-    return res.send("Delete Video");
+const fakeUser = {
+  username: "SOO",
+  loggedIn: false,
+};
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", fakeUser: fakeUser });
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.send("Upload");
+export const deleteVideo = (req, res) => {
+  console.log(req.params);
+  return res.send("Delete Video");
 };
