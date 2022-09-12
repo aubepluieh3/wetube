7cf7955211d1fd57b4cebebdea4a5b1cb1f7c05d
SOO <66086544+aubepluieh3@users.noreply.github.com>
2022-09-12 21:01:53 +0900

    #5.19 Middlewares

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -27,7 +27,7 @@ export const getEdit = async (req, res) => {
 export const postEdit = async (req, res) => {
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
-  const video = await Video.exists({_id:id});
+  const video = await Video.exists({ _id: id });
   if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
   }
@@ -51,9 +51,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags
-        .split(",")
-        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+      hashtags,
     });
     return res.redirect("/");
   } catch (error) {
