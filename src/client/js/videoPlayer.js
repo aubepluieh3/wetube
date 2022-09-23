135b143df88c5b25338e28fc7892775147a32452
SOO <66086544+aubepluieh3@users.noreply.github.com>
2022-09-23 16:54:57 +0900

    #10.1 Play Pause

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,23 @@
 console.log("video player");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const musteBtn = document.getElementById("mute");
+const time = document.getElementById("mute");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "Pause");
+const handleMute = (e) => {};
+
+playBtn.addEventListener("click", handlePlayClick);
+musteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
